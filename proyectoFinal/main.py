635c9b1dfb6bceda543de2700e05483abdad28da
foramirez50 <98720686+foramirez50@users.noreply.github.com>
2022-11-30 21:46:15 -0500

    Update main.py

--- proyectoFinal/main.py
+++ proyectoFinal/main.py
@@ -5,7 +5,6 @@ import numpy as np
 
 st.header("Predicción de tarifa")
 st.text_input("Enter your Name: ", key="name")
-data = pd.read_csv(r"C:\Users\USER\Desktop\FishWeightPredictionApplication-master\nueva_entrada.csv", sep=",", encoding="utf-8")
 
 
 # load model
@@ -16,14 +15,9 @@ best_model = joblib.load('my_model_knn.pkl')
 #st.subheader("Please select relevant features of your fish!")
 #left_column, right_column = st.columns(2)
 
-print(data)
-
 
 if st.button('Make Prediction'):
-    inputs = data
+    inputs = pd.read_csv(r"C:\Users\USER\Desktop\FishWeightPredictionApplication-master\nueva_entrada.csv", sep=",", encoding="utf-8")
     prediction = best_model.predict(inputs)
     print("Tarifa", prediction)
     st.write(f"Su Tarifa es de : {prediction}g")
-
-
-
