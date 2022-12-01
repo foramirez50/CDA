80ab3b09db881e653022f8abfd613f423bbfc916
foramirez50 <98720686+foramirez50@users.noreply.github.com>
2022-11-30 21:26:04 -0500

    subir carpeta

---
+++ proyectoFinal/main.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+st.header("Predicción de tarifa")
+st.text_input("Enter your Name: ", key="name")
+data = pd.read_csv(r"C:\Users\USER\Desktop\FishWeightPredictionApplication-master\nueva_entrada.csv", sep=",", encoding="utf-8")
+
+
+# load model
+import joblib
+best_model = joblib.load('my_model_knn.pkl') 
+
+
+#st.subheader("Please select relevant features of your fish!")
+#left_column, right_column = st.columns(2)
+
+print(data)
+
+
+if st.button('Make Prediction'):
+    inputs = data
+    prediction = best_model.predict(inputs)
+    print("Tarifa", prediction)
+    st.write(f"Su Tarifa es de : {prediction}g")
+
+
+
